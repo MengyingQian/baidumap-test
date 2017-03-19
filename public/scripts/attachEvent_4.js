2e14408a91f67541198dbcef132f67e67a3dab28
jingruoyu <jingruoyu@163.com>
2017-03-19 12:20:14 +0800

    'vue_test'

--- public/scripts/attachEvent_4.js
+++ public/scripts/attachEvent_4.js
@@ -4,7 +4,17 @@
 addLoadEvent(setTab);
 //获取时间跨度，设置默认时间
 addLoadEvent(function(){
-    getdata('/aggregate',{system:'LTE',attr:'业务时间'})
+    /*地图显示设置*/
+    var map = new BMap.Map("allmap",{enableMapClick:false});
+    var point = new BMap.Point(106.76355,26.634111);
+    map.centerAndZoom(point, 15);
+    map.enableScrollWheelZoom(true);
+    var top_right_navigation = new BMap.NavigationControl({anchor: BMAP_ANCHOR_TOP_RIGHT, type: BMAP_NAVIGATION_CONTROL_SMALL}); //右上角，添加默认缩放平移控件     
+    map.addControl(top_right_navigation); 
+    var oldMarker;
+    var Maxtime;
+    var Mintime;
+    /*getdata('/aggregate',{system:'LTE',attr:'业务时间'})
     .then(function(dbdata){
         Maxtime = dbdata.Maxtime;
         Mintime = dbdata.Mintime;
@@ -12,7 +22,7 @@ addLoadEvent(function(){
         $("#tl").attr("placeholder",Mintime.split('-')[0].split(' ')[1]);
         $("#dh").attr("placeholder",Maxtime.split('-')[1].split(' ')[0]);
         $("#th").attr("placeholder",Maxtime.split('-')[1].split(' ')[1]);
-    });
+    });*/
 });
 
 /*属性查询触发*/
--- public/scripts/attachEvent_6.js
+++ public/scripts/attachEvent_6.js
@@ -7,11 +7,22 @@
 });*/
 //获取时间跨度
 addLoadEvent(function(){
-	getdata('/aggregate',{system:'LTE',attr:'业务时间'})
+	/*地图显示设置*/
+	var map = new BMap.Map("allmap",{enableMapClick:false});
+	var point = new BMap.Point(106.76355,26.634111);
+	map.centerAndZoom(point, 15);
+	map.enableScrollWheelZoom(true);
+	var top_right_navigation = new BMap.NavigationControl({anchor: BMAP_ANCHOR_TOP_RIGHT, type: BMAP_NAVIGATION_CONTROL_SMALL}); //右上角，添加默认缩放平移控件     
+	map.addControl(top_right_navigation); 
+	var top_right_control = new BMap.ScaleControl({anchor: BMAP_ANCHOR_TOP_RIGHT });// 左下角，添加比例尺
+	map.addControl(top_right_control); 
+	var oldTangle;
+	var oldColor;
+	/*getdata('/aggregate',{system:'LTE',attr:'业务时间'})
 	.then(function(dbdata){
 		Maxtime = dbdata.Maxtime;
 		Mintime = dbdata.Mintime;
-	});
+	});*/
 /*	var bs = map.getBounds();   //获取可视区域
 	var bssw = bs.getSouthWest();   //可视区域左下角
 	var bsne = bs.getNorthEast();   //可视区域右上角
